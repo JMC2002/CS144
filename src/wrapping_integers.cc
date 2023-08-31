5e5da9320e80414b44ff276766e8e68b12bb077d
MC J <jmc8@qq.com>
2023-09-01 01:52:07 +0800

    完成wrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,19 +1,14 @@
 ﻿#include "wrapping_integers.hh"
-
+#include <algorithm>
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return zero_point + n;
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint64_t const dis = raw_value_ - wrap( checkpoint, zero_point ).raw_value_;
+  return checkpoint + ( dis <= ( 1u << 31 ) ? dis : min( dis, -dis ) );
 }
