ee8b55cef9ea0a05975183cdb74d479009f18eec
MC J <jmc8@qq.com>
2023-09-10 17:39:33 +0800

    改一下

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,5 +1,6 @@
 ﻿#include "wrapping_integers.hh"
 #include <algorithm>
+#include <string>
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
@@ -10,5 +11,8 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   uint64_t const dis = raw_value_ - wrap( checkpoint, zero_point ).raw_value_;
-  return checkpoint + ( dis <= ( 1u << 31 ) ? dis : min( dis, -dis ) );
-}
+  if ( dis <= 1ul << 31 || checkpoint + dis < 1ul << 32 )
+    return checkpoint + dis;
+  else return checkpoint + dis - ( 1ul << 32 );
+  //return checkpoint + ( dis <= ( 1u << 31 ) ? dis : min( dis, -dis ) );
+} 
